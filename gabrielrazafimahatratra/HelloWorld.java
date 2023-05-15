9c373a8053e7ed6d4d7afbfd7966cdfc2c39eb01
Fifaliana Gabriel <gabrielrazafimahatratra@gmail.com>
2023-05-15 10:17:49 +0300

    Ajout de mon packetage

---
+++ gabrielrazafimahatratra/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.gabrielrazafimahatratra;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bonjour, j'utilise eclipse");
+	}
+
+}
